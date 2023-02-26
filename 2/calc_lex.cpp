064585e6bb7bc9f21c6133d9802e074754f3bc8b
mitsu1119 <hachimitsu1119@gmail.com>
2023-02-27 07:37:13 +0900

    calc

---
+++ 2/calc_lex.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_TEXT 100
+
+void error(const char *str) {
+	fprintf(stderr, str);
+	exit(1);
+}
+
+bool isspace(char c) {
+	if(c == ' ' || c == '	') return true;
+	return false;
+}
+
+bool isalpha(char c) {
+	if(c >= 'A' && c <= 'Z') return true;
+	if(c >= 'a' && c <= 'z') return true;
+	return false;
+}
+
+bool isdigit(char c) {
+	if(c >= '0' && c <= '9') return true;
+	return false;
+}
+
+int main() {
+	char c;
+	char text[MAX_TEXT] = {0};
+	int text_cnt, n;
+
+	// interpreter loop
+	while(true) {
+		printf(">> ");
+
+		// tokens loop
+		while(true) {
+			c = getc(stdin);
+
+			if(isspace(c)) continue;
+			if(c == '\n') {
+				printf("\n");
+				break;
+			}
+
+			switch(c) {
+				case '+':
+					printf("PLUS ");
+					continue;
+				case '-':
+					printf("MINUS ");
+					continue;
+			}
+
+			// input token loop
+			text_cnt = 0;
+			if(isalpha(c)) {
+				do {
+					text[text_cnt++] = c;
+					c = getc(stdin);
+				} while(isalpha(c));
+
+				text[text_cnt] = 0;
+				ungetc(c, stdin);
+				
+				if(!strcmp(text, "xor")) printf("XOR ");
+				else error("syntax error\n");
+			} else if(isdigit(c)) {
+				n = 0;
+				do {
+					n = n * 10 + c - '0';
+					c = getc(stdin);
+				} while(isdigit(c));
+				ungetc(c, stdin);
+				printf("NUMBER(%d) ", n);
+			} else {
+				error("syntax error\n");
+			}
+		}
+	}
+
+	return 0;
+}
